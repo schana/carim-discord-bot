38e46582d4430ca6dc96a1256ca4d17871c3ebbc
Nathaniel Schaaf <schaafna@gmail.com>
2020-04-11 21:57:46 +0200

    Add test configuration

--- src/carim_discord_bot/setup_instructions.py
+++ src/carim_discord_bot/setup_instructions.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from pkg_resources import resource_filename
 
---
+++ tests/test_instructions.py
@@ -0,0 +1,5 @@
+from carim_discord_bot import setup_instructions
+
+
+def test_integration_setup():
+    setup_instructions.print_setup_instructions()
