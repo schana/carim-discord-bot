ad75afd2cd5f4199b195ab2390f32896d39c2df4
Nathaniel Schaaf <schaafna@gmail.com>
2020-06-13 22:38:17 +0200

    Added player count and log discord service messages

--- src/carim_discord_bot/discord_client/discord_service.py
+++ src/carim_discord_bot/discord_client/discord_service.py
@@ -1,12 +1,31 @@
+import datetime
+
+import discord
+
 from carim_discord_bot import managed_service, config
 from carim_discord_bot.discord_client import client
 from carim_discord_bot.managed_service import Message
+
+
+class PlayerCount(managed_service.Message):
+    def __init__(self, count, slots):
+        super().__init__()
+        self.count = count
+        self.slots = slots
+
+
+class Log(managed_service.Message):
+    def __init__(self, text):
+        super().__init__()
+        self.text = text
 
 
 class DiscordService(managed_service.ManagedService):
     def __init__(self):
         super().__init__()
         self.client = None
+        self.log_rollup = []
+        self.last_log_time = datetime.datetime.now()
 
     async def stop(self):
         await self.client.close()
@@ -19,6 +38,19 @@ class DiscordService(managed_service.ManagedService):
 
     async def handle_message(self, message: Message):
         await self.client.wait_until_ready()
+        if isinstance(message, PlayerCount):
+            channel: discord.TextChannel = self.client.get_channel(config.get().count_channel_id)
+            player_count_string = config.get().player_count_format.format(players=message.count,
+                                                                          slots=message.slots)
+            await channel.edit(name=player_count_string)
+        elif isinstance(message, Log):
+            self.log_rollup.append(message.text)
+            if datetime.timedelta(seconds=10) < datetime.datetime.now() - self.last_log_time:
+                channel: discord.TextChannel = self.client.get_channel(config.get().rcon_admin_log_channel_id)
+                rolled_up_log = '\n'.join(self.log_rollup)
+                await channel.send(f'```{rolled_up_log}```')
+                self.last_log_time = datetime.datetime.now()
+                self.log_rollup = list()
 
 
 service = None
--- src/carim_discord_bot/steam/steam_service.py
+++ src/carim_discord_bot/steam/steam_service.py
@@ -1,5 +1,11 @@
-from carim_discord_bot import managed_service
+import asyncio
+import logging
+
+from carim_discord_bot import managed_service, config
+from carim_discord_bot.discord_client import discord_service
 from carim_discord_bot.steam import query
+
+log = logging.getLogger(__name__)
 
 
 class Query(managed_service.Message):
@@ -10,13 +16,35 @@ class Query(managed_service.Message):
 
 
 class SteamService(managed_service.ManagedService):
+    def __init__(self):
+        super().__init__()
+        self.current_count = -1
 
     async def handle_message(self, message: managed_service.Message):
         if isinstance(message, Query):
             await query.query(message.ip, message.port, message.result)
 
     async def service(self):
-        pass
+        while True:
+            await asyncio.sleep(config.get().update_player_count_interval)
+            await self.update_player_count()
+
+    async def update_player_count(self):
+        message = Query(config.get().ip, config.get().steam_port)
+        await self.send_message(message)
+        try:
+            result = await asyncio.wait_for(message.result, 10)
+        except (asyncio.TimeoutError, asyncio.CancelledError):
+            log.warning('update player count query timed out')
+            return
+
+        result: query.SteamData = result
+        count_players = result.players
+        if count_players != self.current_count:
+            message = discord_service.PlayerCount(count_players, result.max_players)
+            message = discord_service.Log(f'Update player count: {count_players}/{result.max_players}')
+            await discord_service.get_service_manager().send_message(message)
+            self.current_count = count_players
 
 
 service = None
