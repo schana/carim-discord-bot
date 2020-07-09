fe263f7fb3aeba5096dfe939f9c808cbe4180158
Nathaniel Schaaf <nathaniel.schaaf@gmail.com>
2020-07-09 19:19:34 +0200

    Update discord library version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     install_requires=[
-        'discord.py>=1.3.2,<1.4.0a0'
+        'discord.py>=1.3.4,<1.4.0a0'
     ],
     extras_require={
         'tests': [
